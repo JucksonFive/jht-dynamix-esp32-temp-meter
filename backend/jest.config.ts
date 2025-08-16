58bdad9b5e0532ab96dc4082c646881630c4c3ac
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2025-08-16 20:07:08 +0300

    feat: add coverage configuration to Jest for improved test reporting

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -4,7 +4,16 @@ const config: Config = {
   testEnvironment: "node",
   testMatch: ["**/*.spec.ts"],
   clearMocks: true,
-  // Poista outDir (dist) Jestin poluilta:
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "<rootDir>/lambdas/**/*.ts",
+    "!<rootDir>/**/*.spec.ts",
+    "!<rootDir>/cdk/**",
+    "!<rootDir>/dist/**",
+  ],
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  coveragePathIgnorePatterns: ["node_modules", "dist"],
 };
 export default config;
