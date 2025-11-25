811e2b3837efe307eebf3637ebd6a07afa2a1fad
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2025-11-25 12:24:17 +0200

    refactor: remove unused social media icons from Footer component

--- homepage/src/components/Footer.tsx
+++ homepage/src/components/Footer.tsx
@@ -2,30 +2,6 @@ import React from "react";
 import { useI18n } from "../locales/I18nProvider";
 import { Link } from "./Link";
 
-const LinkedinIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className="h-5 w-5"
-    aria-hidden="true"
-  >
-    <path d="M4.98 3.5a2.5 2.5 0 1 1-.02 5 2.5 2.5 0 0 1 .02-5Zm.02 5.75H2V21h3V9.25Zm5 0H7V21h3v-5.8c0-1.54.41-2.42 1.66-2.42 1.27 0 1.35 1.04 1.35 2.5V21h3v-6.27c0-3.02-.64-5.23-4.08-5.23-1.66 0-2.78.91-3.24 1.77h-.04l-.17-1.52Z" />
-  </svg>
-);
-
-const GithubIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className="h-5 w-5"
-    aria-hidden="true"
-  >
-    <path d="M12 .5a11.5 11.5 0 0 0-3.64 22.41c.58.1.79-.24.79-.54v-1.88c-3.2.7-3.87-1.54-3.87-1.54-.52-1.33-1.26-1.68-1.26-1.68-1.03-.7.08-.68.08-.68 1.14.08 1.75 1.17 1.75 1.17 1.01 1.72 2.66 1.22 3.31.93.1-.73.4-1.22.72-1.5-2.55-.29-5.23-1.28-5.23-5.68 0-1.26.45-2.29 1.16-3.1-.12-.29-.51-1.47.11-3.06 0 0 .96-.31 3.14 1.18a10.78 10.78 0 0 1 5.72 0c2.18-1.49 3.14-1.18 3.14-1.18.62 1.59.23 2.77.11 3.06.72.81 1.16 1.84 1.16 3.1 0 4.41-2.68 5.38-5.24 5.67.41.35.77 1.04.77 2.1v3.11c0 .3.21.65.8.54A11.5 11.5 0 0 0 12 .5Z" />
-  </svg>
-);
-
 export const Footer: React.FC = () => {
   const { t } = useI18n();
   const year = new Date().getFullYear();
@@ -134,7 +110,6 @@ export const Footer: React.FC = () => {
             </div>
           </div>
         </div>
-        {/* Bottom bar: only copyright */}
         <div className="mt-12 border-t border-slate-800/60 pt-6 text-xs text-center text-slate-500">
           {t.app.footer.copyright(year)}
         </div>
