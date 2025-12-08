23e4919361a13291301c7ab8468e0d1c96f967ac
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2025-12-08 09:00:21 +0200

    refactor: reorganize TempSensor class methods for improved readability

--- board/lib/temperature_sensor/temperature_sensor.h
+++ board/lib/temperature_sensor/temperature_sensor.h
@@ -5,9 +5,11 @@
 
 class TempSensor
 {
+private:
+    static void publishTemperature(float temperature, OfflineSyncHelper offlineSync, String mqtt_topic_str, String userId, String deviceId);
+    static float readCelsius();
+
 public:
     static void setup();
-    static float readCelsius();
-    static void publishTemperature(float temperature, OfflineSyncHelper offlineSync, String mqtt_topic_str, String userId, String deviceId);
     static void publishTemperatureIfDue(OfflineSyncHelper offlineSync, String mqtt_topic_str, String userId, String deviceId);
 };
