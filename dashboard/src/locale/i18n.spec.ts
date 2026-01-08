f273e8990d3edc1a5ead38561698e0c630e331fc
Hizaguru <jukka-pekka.lappalainen@outlook.com>
2026-01-08 12:12:24 +0200

    refactor: rename dashboard references to dynlink

--- dashboard/src/locale/i18n.spec.ts
+++ dashboard/src/locale/i18n.spec.ts
@@ -3,6 +3,6 @@ import i18n from "src/locale/i18n";
 describe("locale/i18n.ts", () => {
   it("initializes with English resources", () => {
     expect(i18n.language).toBe("en");
-    expect(i18n.t("appTitle")).toBe("JT-DYNAMIX Dashboard");
+    expect(i18n.t("appTitle")).toBe("JT-DYNAMIX Dynlink");
   });
 });
